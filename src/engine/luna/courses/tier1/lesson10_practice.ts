343dcb8649b51026daf4fee32de7b2a0b357d301
freelancerschmidt2026-coder <freelancerschmidt2026@gmail.com>
2026-04-15 21:34:17 -0400

    Create lesson10_practice.ts

---
+++ src/engine/luna/courses/tier1/lesson10_practice.ts
@@ -0,0 +1,9 @@
+import { MultipleChoiceQuestion } from "../types";
+
+export const lesson10_practice = {
+  id: "lesson10_practice",
+  title: "Practice Readings",
+  body: `
+Practice is the key to becoming a confident tarot reader. In this lesson, you will learn how to practice readings safely, effectively, and intuitively.
+
+Practice
