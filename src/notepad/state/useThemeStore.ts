146ef846faa58926839209d1984dd69b51508f80
freelancerschmidt2026-coder <freelancerschmidt2026@gmail.com>
2026-04-20 01:16:01 -0400

    Update useThemeStore.ts

--- src/notepad/state/useThemeStore.ts
+++ src/notepad/state/useThemeStore.ts
@@ -4,9 +4,16 @@ import { NotebookThemeId } from "../systems/themeSystem";
 interface ThemeState {
   themeId: NotebookThemeId;
   setTheme: (id: NotebookThemeId) => void;
+  setThemeFromLuna: (id: NotebookThemeId) => void;
 }
 
 export const useThemeStore = create<ThemeState>((set) => ({
   themeId: "void",
+
   setTheme: (id) => set({ themeId: id }),
+
+  setThemeFromLuna: (id) =>
+    set({
+      themeId: id,
+    }),
 }));
