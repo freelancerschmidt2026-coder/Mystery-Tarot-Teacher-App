8f391d8786c96790eb65e9ee8b8cce1e53f9f2ff
freelancerschmidt2026-coder <freelancerschmidt2026@gmail.com>
2026-04-22 12:20:22 -0400

    Update index.ts

--- src/engine/luna/index.ts
+++ src/engine/luna/index.ts
@@ -1,1 +1,47 @@
+// src/engine/luna/index.ts
 
+// FOUNDATION
+export { default as PathSanitizationEngine } from "./PathSanitizationEngine";
+export { default as SafeNameEngine } from "./SafeNameEngine";
+export { default as StorageKeyValidator } from "./StorageKeyValidator";
+export { default as IdNormalizationEngine } from "./IdNormalizationEngine";
+export { default as FileIntegrityEngine } from "./FileIntegrityEngine";
+
+// CORE STUDIO ENGINES
+export { default as StudioDeckEngine } from "./StudioDeckEngine";
+export { default as TarotBoxEngine } from "./TarotBoxEngine";
+export { default as CardFlipEngine } from "./CardFlipEngine";
+export { default as CardArtEngine } from "./CardArtEngine";
+export { default as TemplateApplicationEngine } from "./TemplateApplicationEngine";
+export { default as TemplateRotationEngine } from "./TemplateRotationEngine";
+export { default as TemplateVariantEngine } from "./TemplateVariantEngine";
+export { default as TemplateMetadataEngine } from "./TemplateMetadataEngine";
+export { default as DeckBoxAnimationEngine } from "./DeckBoxAnimationEngine";
+export { default as DeckProgressEngine } from "./DeckProgressEngine";
+export { default as StudioUndoRedoEngine } from "./StudioUndoRedoEngine";
+export { default as StudioAutosaveEngine } from "./StudioAutosaveEngine";
+export { default as StudioEventEngine } from "./StudioEventEngine";
+export { default as VoiceCommandEngine } from "./VoiceCommandEngine";
+
+// INTELLIGENCE LAYER
+export { default as EmotionEngine } from "./EmotionEngine";
+export { default as SuggestionEngine } from "./SuggestionEngine";
+export { default as CreativeDirectionEngine } from "./CreativeDirectionEngine";
+export { default as CardMeaningAnalysisEngine } from "./CardMeaningAnalysisEngine";
+export { default as DeckConsistencyEngine } from "./DeckConsistencyEngine";
+export { default as DesignSessionEngine } from "./DesignSessionEngine";
+export { default as ThemeEngine } from "./ThemeEngine";
+
+// ECONOMY + STORE
+export { default as MysteryTokenEngine } from "./MysteryTokenEngine";
+export { default as MemberEconomyLedgerEngine } from "./MemberEconomyLedgerEngine";
+export { default as StoreListingEngine } from "./StoreListingEngine";
+export { default as MysteryDeckApprovalEngine } from "./MysteryDeckApprovalEngine";
+export { default as TemplatePricingEngine } from "./TemplatePricingEngine";
+
+// NOTEPAD + EXPORT
+export { default as NotepadIntegrationEngine } from "./NotepadIntegrationEngine";
+export { default as DeckExportEngine } from "./DeckExportEngine";
+
+// BREAKOUT ROOMS
+export { default as BreakoutRoomEngine } from "./BreakoutRoomEngine";
